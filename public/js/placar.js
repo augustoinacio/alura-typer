ecdbf39b63c5d9e41c23fb41b0330f5f83d5c8a4
Augusto Inacio <augustoinacio@gmail.com>
2017-12-11 12:23:20 -0200

    Alteracoes parte 2

--- public/js/placar.js
+++ public/js/placar.js
@@ -1,3 +1,5 @@
+$("#botao-placar").click(mostraPlacar);
+
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
     var numPalavras = $("#contador-palavras").text();
@@ -5,6 +7,15 @@ function inserePlacar() {
     var linha = novaLinha(usuario, numPalavras);
     linha.find(".botao-remover").click(removeLinha);
     corpoTabela.prepend(linha);
+    $(".placar").slideDown(500);
+    scrollPlacar();
+}
+
+function scrollPlacar() {
+    var posicaoPlacar = $(".placar").offset().top;
+    $("body").animate({
+        scrollTop: posicaoPlacar + "px"
+    }, 1000);
 }
 
 function novaLinha(usuario, numPalavras) {
@@ -12,9 +23,9 @@ function novaLinha(usuario, numPalavras) {
     var colunaUsuario = $("<td>").text(usuario);
     var colunaNumPalavras = $("<td>").text(numPalavras);
     var colunaRemover = $("<td>");
-    var link = $("<a>").addClass("botao-remover").attr("href","#");
+    var link = $("<a>").addClass("botao-remover").attr("href", "#");
     var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
-    link.append(icone);     
+    link.append(icone);
     colunaRemover.append(link);
     linha.append(colunaUsuario);
     linha.append(colunaNumPalavras)
@@ -22,8 +33,15 @@ function novaLinha(usuario, numPalavras) {
     return linha;
 }
 
-function removeLinha(){
+function removeLinha() {
     event.preventDefault();
-    $(this).parent().parent().remove();
+    var linha = $(this).parent().parent();
+    linha.fadeOut(1000);
+    setTimeout(function () {
+        linha.remove();
+    }, 1000)
+}
 
+function mostraPlacar() {
+    $(".placar").stop().slideToggle(600);
 }
